392c72663492a5890444d440ba6ce9c7455b1e39
lmonteiro18 <luis-eduardo@live.com.pt>
2022-05-03 15:58:43 +0100

    Comecei a tentar passar os dados para o frontend

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 require('dotenv').config();
 var request = require('request');
 var querystring = require('querystring');
 const spotifyWebApi = require('spotify-web-api-node');
 
+//local storage
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch', Number.MAX_VALUE);
+}
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -15,15 +22,17 @@ app.use(express.static("public"));
 
 const auth_url = "https://accounts.spotify.com/authorize";
 const token_url = "https://accounts.spotify.com/api/token";
-const redirect_url = "http://localhost:3000/access";
+const redirect_url = "http://localhost:3000/access"; //TODO: mudar redirect_url quando pusermos o site online
 
 let spotifyApi;
 
-let user_playlists;
+let user_playlists = [];
+let user_savedTracks = [];
 
 app.route("/")
   .get(function(req, res) {
-    res.sendFile(__dirname + "/public/html/index.html");
+    res.render("index", {});
+    //res.sendFile(__dirname + "/public/html/index.html");
   });
 
 app.route("/auth")
@@ -46,13 +55,6 @@ app.route("/auth")
     let authorizeURL = spotifyApi.createAuthorizeURL(scopes);
     //console.log("Authorize Url: " + authorizeURL);
     res.redirect(authorizeURL);
-
-    /*res.redirect(auth_url + querystring.stringify({
-      response_type: 'code',
-      client_id: spotifyApi.getClientId(),
-      scope: scopes,
-      redirect_uri: spotifyApi.getRedirectURI()
-    }));*/
   });
 
 app.route("/access")
@@ -91,26 +93,57 @@ app.route("/access")
 
         let my_id;
 
+        //ir buscar informações do utilizador (aqui é apenas para ir buscar o id dele para uso futuro)
         spotifyApi.getMe()
           .then(function(data) {
-            //console.log(data.body);
             my_id = data.body.id;
-            //console.log("My ID: " + my_id);
           }, function(err) {
-            //console.log("Something went wrong!", err);
+            console.log("Something went wrong!", err);
           });
 
+        let limit = 50; //maximo aceite (o spotify tem um sistema de paginação em que só se consegue ir buscar 50 items de cada vez)
+
+        //ir buscar playlists do utilizador
         spotifyApi.getUserPlaylists(my_id)
           .then(function(data) {
-            //console.log('Retrieved playlists', data.body);
-            user_playlists = data.body.items;
-            //console.log("Playlists: " + user_playlists);
-            res.redirect('/homeview');
+            let total_playlists = data.body.total;
+            return total_playlists;
+            console.log(data.body);
+            let playlists = [];
+            (data.body.items).map(playlist => {
+
+            });
           }, function(err) {
-            //console.log('Something went wrong!', err);
+            console.log('Something went wrong!', err);
+          }).then(function(total_playlists){
+            let playlists;
+            let savePlaylists = async function() {
+              playlists = await getPlaylists(total_playlists, my_id, limit);
+            }
+            savePlaylists();
           });
 
-
+        //ir buscar saved tracks do utilizador
+        //até ao segundo then (exclusive) é só para ir buscar o número total de tracks
+        //para depois poder utilizar na função lá de baixo
+        spotifyApi.getMySavedTracks({
+            limit: 1,
+            offset: 0
+          })
+          .then(function(data) {
+            let total_tracks = data.body.total;
+            return total_tracks;
+          }, function(err) {
+            console.log('Something went wrong!', err);
+          }).then(function(total_tracks) {
+            let tracks;
+            let saveTracks = async function() {
+              tracks = await getSavedTracks(total_tracks, limit);
+            }
+            saveTracks();
+          }).then(function() {
+            res.redirect('/homeview');
+          });
       }
     });
   });
@@ -143,12 +176,106 @@ app.route("/access")
 
 app.route("/homeview")
   .get(function(req, res) {
-    user_playlists.map((playlist) => console.log(playlist.name));
-    res.sendFile(__dirname + "/public/html/homeview.html");
+    //console.log("Playlists: " + JSON.parse(localStorage.user_playlists)[0][0].name);
+    //console.log("Tracks: " + JSON.parse(localStorage.user_savedTracks).length);
+
+    res.render("homeview", {
+      playlists: localStorage.user_playlists,
+      savedTracks: localStorage.user_savedTracks
+    });
+    //res.sendFile(__dirname + "/public/html/homeview.html");
   });
 
+//função para ir buscar e guardar as saved tracks do utilizador
+async function getSavedTracks(total_tracks, limit) {
+  let n_iterations = Math.ceil(total_tracks / limit);
+  for (let i = 0; i < n_iterations; i++) {
+    await spotifyApi.getMySavedTracks({
+        limit: limit,
+        offset: i
+      })
+      .then(function(data) {
+        let savedTracks = [];
+        (data.body.items).map((track, j) => {
+          if (user_savedTracks.length + savedTracks.length < total_tracks) {
+            let filtered_track = {
+              key: i * 50 + j,
+              id: track.track.id,
+              name: track.track.name,
+              popularity: track.track.popularity,
+              explicit: track.track.explicit,
+              duration: track.track.duration_ms,
+              artists: [],
+              saved_date: track.added_at
+            }
+            let getArtists = async function() {
+              let artists = [];
+              await track.track.artists.map(artist => artists.push({
+                id: artist.id,
+                name: artist.name
+              }));
+              return artists;
+            }
+            let saveArtists = async function() {
+              let artists = await getArtists();
+              filtered_track.artists = artists;
+            }
+            saveArtists();
+            savedTracks.push(filtered_track);
+          }
+        });
+        return savedTracks;
+      }, function(err) {
+        console.log('Something went wrong!', err);
+      }).then(function(savedTracks) {
+        savedTracks.map(track => user_savedTracks.push(savedTracks));
+        //console.log("User length: " + user_savedTracks.length);
+      });
+  }
+  localStorage.setItem('user_savedTracks', JSON.stringify(user_savedTracks));
+  //console.log(JSON.parse(localStorage.user_savedTracks)[0][0].artists);
+  //console.log(JSON.parse(localStorage.user_savedTracks).length);
+  user_savedTracks = [];
+}
 
-let port = process.env.PORT || 3000;
+
+//função para ir buscar e guardar as playlists do utilizador
+async function getPlaylists(total_playlists, user_id, limit) {
+  let n_iterations = Math.ceil(total_playlists / limit);
+  for (let i = 0; i < n_iterations; i++) {
+    await spotifyApi.getUserPlaylists(user_id, {limit: limit, offset: i})
+      .then(function(data) {
+        let playlists = [];
+        (data.body.items).map((playlist, j) => {
+          if (user_playlists.length + playlists.length < total_playlists) {
+            let filtered_playlist = {
+              key: i * 50 + j,
+              id: playlist.id,
+              name: playlist.name,
+              description: playlist.description,
+              public: playlist.public,
+              collaborative: playlist.collaborative
+            }
+            playlists.push(filtered_playlist);
+          }
+        });
+        return playlists;
+      }, function(err) {
+        console.log('Something went wrong!', err);
+      }).then(function(playlists) {
+        playlists.map(track => user_playlists.push(playlists));
+        //console.log("Playlists length: " + user_playlists.length);
+      });
+  }
+  localStorage.setItem('user_playlists', JSON.stringify(user_playlists));
+  //console.log(JSON.parse(localStorage.user_playlists));
+  //console.log(JSON.parse(localStorage.user_playlists).length);
+  user_playlists = [];
+}
+
+
+
+let port = process.env.PORT || 3000; //TODO: mudar para a porta correta do localhost (pode ser diferente de pessoa para pessoa, no meu caso é 3000)
 app.listen(port, function(req, res) {
   console.log(`Server up and running on port ${port}.`);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,9 @@
-let client_id = "855793d2a9054fff95e9868a4ada03ce";
-let client_secret = "c1171bfdcd834c5599976d2b8a06403c";
+let user_playlists = [];
 
-const auth_url = "https://accounts.spotify.com/authorize";
-const token_url = "https://accounts.spotify.com/api/token";
-const redirect_url = "http://localhost:3000/";
-
-let id;
-let secret;
-
-
-window.onLoad = onPageLoad();
-
-function onPageLoad(){
-  if(window.location.search.length>0){
-    let code = getCode();
-    getAccess();
+async function savePlaylists(playlists){
+  let parsed_playlists = JSON.parse(playlists);
+  for (let i = 0; i < parsed_playlists.length; i++) {
+    await user_playlists.push(parsed_playlists[0][i]);
+    //console.log(user_playlists.playlists[i][0]);
   }
-}
-
-function reqAuth() {
-  id = document.getElementById("ClientID").value;
-  secret = document.getElementById("ClientSecret").value;
-
-  let url = auth_url;
-  //url += `?client_id=${process.env.CLIENT_ID}`;
-  url += `?client_id=${id}`;
-  url += `&response_type=code`;
-  url += `&redirect_uri=${redirect_url}`;
-  url += `&show_dialog=true`;
-  url += `&scope=playlist-read-private user-library-read user-follow-read user-top-read playlist-read-collaborative user-read-private`;
-
-  location.assign(url);
-}
-
-function getCode(){
-  let searchString = window.location.search;
-  console.log(window.location.search);
-}
-
-function preventSubmit(e){
-  e.preventDefault();
 }
